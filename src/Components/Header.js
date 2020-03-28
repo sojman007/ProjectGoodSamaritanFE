259acc91a7934a30b28ece631c4ecb9f98caa2a8
sojman007 <sojman007@yahoo.com>
2020-03-28 22:49:57 +0100

    Changes made by Soji

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return(
-            <div>
-            <nav className="navbar navbar-expand-lg navbar-dark">
-
+            
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                
                 <a className="navbar-brand" href="/">PGE</a>
 
+                {
+                
+                /* we wont be needing this for now 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
+                    
+                */
 
+                }
 
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-
-                <ul className="navbar-nav ml-auto">
+                    
+                    { /* and this as well
+                    
+                     <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
                         <a className="nav-link" href="#Home">Home</a>
                     </li>
@@ -26,9 +34,11 @@ class Header extends Component {
                         <a className="nav-link" href="#About">About</a>
                     </li>
                 </ul>
-                </div>
+
+                    */}
+                                 </div>
                 </nav>
-            </div>
+            
                 
             
         )
